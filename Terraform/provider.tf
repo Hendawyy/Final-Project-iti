51950f199477918966eac4c6aba894136d14d167
Hendawyy <seifhendawy1@gmail.com>
2023-10-27 11:39:20 +0200

    Update secrets Credentials

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,5 +1,7 @@
 provider "google" {
-  credentials = file(var.sa-key)
+  # credentials = file(var.sa-key)
+  credentials = jsondecode(var.sa-key)
+
 
   project = var.project_id
   region  = var.region
