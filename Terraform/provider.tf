53fcd174798d88f0df3cfefc6c477041951824b6
Hendawyy <seifhendawy1@gmail.com>
2023-10-27 13:53:20 +0200

    Edit secrets Paths

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,9 +1,6 @@
 provider "google" {
-  # credentials = file(var.sa-key)
-  # credentials = jsondecode(var.sa-key)
-
-
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
+  credentials = file(var.sa-key)
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
 }
