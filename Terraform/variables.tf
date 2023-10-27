3b76373313ad7f56afa079cf4fb2483c91a47053
Hendawyy <seifhendawy1@gmail.com>
2023-10-27 15:20:27 +0200

    Edit secrets Paths

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -15,7 +15,7 @@ variable "zone" {
 
 variable "sa-key" {
   type    = string
-  default = "iti-final-hendawy-647b192c2df1.json"
+  default = "/var/lib/jenkins/workspace/Pipeline1-Terraform-Infrastructure-Apply@2/Terraform/iti-final-hendawy-647b192c2df1.json"
 }
 
 variable "region2" {
