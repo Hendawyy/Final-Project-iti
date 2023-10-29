273ea450fc4abea93ddf7bb322b5556d63eee80d
Hendawyy <seifhendawy1@gmail.com>
2023-10-29 16:23:05 +0200

    Clone Repo in VM

--- Jenkins-Pipelines/Pipeline2-Push&Deploy.groovy
+++ Jenkins-Pipelines/Pipeline2-Push&Deploy.groovy
@@ -6,15 +6,6 @@ pipeline {
     }
     
     stages {
-        // stage('Connect to the cluster from the remote local private instance using IAP') {
-        //     steps {
-        //         sh 'echo "steps"'
-        //         sh 'gcloud container clusters get-credentials gcp-k8s --zone europe-west1-b --project final-project-iti-hendawyy --internal-ip'
-        //         sh 'gcloud compute ssh private-vm-instance --tunnel-through-iap --project=final-project-iti-hendawyy --zone=us-east1-b --ssh-flag="-4 -L8888:localhost:8888 -N -q -f"'
-        //         sh 'export HTTPS_PROXY=localhost:8888'
-                
-        //     }
-        // }
 
         stage('Clone Repo') {
             steps {
@@ -56,8 +47,6 @@ pipeline {
 
                         base64 -i svacckey.json > svacckeyz.json
 
-                        ls
-
                         cat svacckeyz.json | docker login -u _json_key_base64 --password-stdin https://us-east1-docker.pkg.dev
 
                         gcloud container clusters update gcp-k8s --zone europe-west1-b --enable-master-global-access
@@ -77,27 +66,29 @@ pipeline {
 
         stage('Deploy App on GKE') {
             steps {
-                script {
-                sh 'hostname -i'
-                def gcpCredentials = credentials('MyGoogleServiceAccountKey') 
-
-                withCredentials([file(credentialsId: 'MyGoogleServiceAccountKey', variable: 'GCP_CREDENTIALS')]) {
-                            
-                sh 'gcloud compute ssh private-vm-instance --project=final-project-iti-hendawyy --zone=us-east1-b --tunnel-through-iap'
-                sh '''gcloud container clusters get-credentials gcp-k8s --zone europe-west1-b --project final-project-iti-hendawyy --internal-ip
-                    hostname -i'''
-                sh 'kubectl get ns'
-                sh 'pwd'
-                sh 'ls'
-                sh 'kubectl apply -f ../Terraform-Infrastructure-Action/Kubernetes/Mongo/'
-                sh 'sleep 45'
-                sh 'kubectl apply -f ../Terraform-Infrastructure-Action/Kubernetes/App/'
-                sh 'kubectl get pods -n mongo -owide'
-                sh 'kubectl get pods -n node -owide'
-                    }
+                script {    
+                    sh 'hostname -i'
+                    def sshCommand = """
+                        gcloud compute ssh private-vm-instance --project=final-project-iti-hendawyy --zone=us-east1-b --tunnel-through-iap << EOF
+                        hostname -i
+                        gcloud container clusters get-credentials gcp-k8s --zone europe-west1-b --project final-project-iti-hendawyy --internal-ip
+                        hostname -i
+                        kubectl get ns
+                        pwd
+                        git clone https://github.com/Hendawyy/Final-Project-iti
+                        cd Final-Project-iti
+                        ls
+                        kubectl apply -f Kubernetes/Mongo/
+                        sleep 45
+                        kubectl apply -f /Kubernetes/App/
+                        kubectl get pods -n mongo -owide
+                        kubectl get pods -n node -owide
+                        EOF
+                        """
+                    sh sshCommand
                 }
             }
-        }
+}
 
     }
 }
