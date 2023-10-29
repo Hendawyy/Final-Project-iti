a0e930500388fbb2c9d258929b230bb2cd9f67ef
Hendawyy <seifhendawy1@gmail.com>
2023-10-29 14:00:20 +0200

    Add Remote Images

--- Jenkins-Pipelines/Pipeline2-Push&Deploy.groovy
+++ Jenkins-Pipelines/Pipeline2-Push&Deploy.groovy
@@ -8,6 +8,7 @@ pipeline {
     stages {
         stage('Connect to the cluster from the remote local private instance using IAP') {
             steps {
+                sh 'echo "steps"'
                 sh 'gcloud container clusters get-credentials gcp-k8s --zone europe-west1-b --project final-project-iti-hendawyy --internal-ip'
                 sh 'gcloud compute ssh private-vm-instance --tunnel-through-iap --project=final-project-iti-hendawyy --zone=us-east1-b --ssh-flag="-4 -L8888:localhost:8888 -N -q -f"'
                 sh 'export HTTPS_PROXY=localhost:8888'
@@ -76,18 +77,26 @@ pipeline {
 
         stage('Deploy App on GKE') {
             steps {
+                script {
                 sh 'hostname -i'
+                def gcpCredentials = credentials('MyGoogleServiceAccountKey') 
+
+                withCredentials([file(credentialsId: 'MyGoogleServiceAccountKey', variable: 'GCP_CREDENTIALS')]) {
+                            
+                sh 'gcloud auth activate-service-account --key-file=${GCP_CREDENTIALS}'
+                sh 'gcloud config set account seifhendawy1@gmail.com'
+                sh 'gcloud auth activate-service-account final-project-iti-hendawyy-svc@final-project-iti-hendawyy.iam.gserviceaccount.com --key-file=${GCP_CREDENTIALS}'
                 sh 'gcloud container clusters get-credentials gcp-k8s --zone europe-west1-b --project final-project-iti-hendawyy --internal-ip'
                 sh 'gcloud compute ssh private-vm-instance --tunnel-through-iap --project=final-project-iti-hendawyy --zone=us-east1-b --ssh-flag="-4 -L8888:localhost:8888 -N -q -f"'
                 sh 'export HTTPS_PROXY=localhost:8888'
-                sh 'export KUBECONFIG=$HOME/.kube/config'
-                sh 'kubectl config view'
                 sh 'kubectl get ns'
                 sh 'kubectl apply -f ../Terraform-Infrastructure-Action/Kubernetes/Mongo/'
                 sh 'sleep 45'
                 sh 'kubectl apply -f ../Terraform-Infrastructure-Action/Kubernetes/App/'
                 sh 'kubectl get pods -n mongo -owide'
                 sh 'kubectl get pods -n node -owide'
+                    }
+                }
             }
         }
 
