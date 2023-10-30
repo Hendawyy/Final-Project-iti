de58eb873126d3db944adb614f055700ece8f2a6
Hendawyy <seifhendawy1@gmail.com>
2023-10-30 17:39:20 +0200

    Deploy App on GKE & Add Screenshots

--- Jenkins-Pipelines/Pipeline2-Push&Deploy.groovy
+++ Jenkins-Pipelines/Pipeline2-Push&Deploy.groovy
@@ -64,27 +64,37 @@ pipeline {
             }
         }
 
-        stage('Deploy App on GKE') {
+        stage('Upload App on Private VM') {
             steps {
-                script {
-def sshCommand = """
-                    gcloud compute ssh private-vm-instance --project=final-project-iti-hendawyy --zone=us-east1-b --tunnel-through-iap <<EOF
-                    gcloud container clusters get-credentials gcp-k8s --zone europe-west1-b --project final-project-iti-hendawyy --internal-ip
-                    tar -xzvf Kubernetes.tar.gz
-                    rm -rf Kubernetes.tar.gz
-                    kubectl get ns
-                    kubectl apply -f Kubernetes/Mongo/
-                    sleep 45
-                    kubectl apply -f Kubernetes/App/
-                    sleep 10
-                    kubectl get nodes
-                    kubectl get pods -n mongo -owide
-                    kubectl get pods -n node -owide
-EOF
-"""
-                    sh sshCommand
+                sh 'sudo tar -czvf /tmp/Kubernetes.tar.gz -C /home/seif/Documents/Projects/Final_Project/Final-Project-iti/ Kubernetes'
+                sh 'gcloud compute scp --project=final-project-iti-hendawyy --zone=us-east1-b /tmp/Kubernetes.tar.gz seif@private-vm-instance:/home/seif/Kubernetes.tar.gz --tunnel-through-iap'
+                sh 'rm -rf /tmp/Kubernetes.tar.gz'
                 }
             }
+
+        stage('Deploy App on GKE') {
+    steps {
+        script {
+            def sshCommand = """
+                gcloud compute ssh private-vm-instance --project=final-project-iti-hendawyy --zone=us-east1-b --tunnel-through-iap <<EOF
+                ls
+                sudo tar -xzvf /home/seif/Kubernetes.tar.gz -C /home/seif/
+                sudo rm -rf /home/seif/Kubernetes.tar.gz
+                gcloud container clusters get-credentials gcp-k8s --zone europe-west1-b --project final-project-iti-hendawyy --internal-ip
+                kubectl get ns
+                kubectl apply -f /home/seif/Kubernetes/Mongo/
+                sleep 45
+                kubectl apply -f /home/seif/Kubernetes/App/
+                sleep 10
+                kubectl get nodes
+                kubectl get pods -n mongo -owide
+                kubectl get pods -n node -owide
+EOF
+            """
+            sh sshCommand
         }
+    }
+}
+
     }
 }
